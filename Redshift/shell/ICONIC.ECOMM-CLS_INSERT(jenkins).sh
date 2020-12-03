37b696de1273f428e0bd858cd3d98fd1a2919843
alrali <75405592+alrali@users.noreply.github.com>
2020-12-03 14:36:01 +1100

    Delete ICONIC.ECOMM-CLS_INSERT(jenkins).sh

--- Redshift/shell/ICONIC.ECOMM-CLS_INSERT(jenkins).sh
+++
@@ -1,6 +0,0 @@
-#ENV=dev
-set +x
-################ Execute clean staging data ################
-echo "Environment name is : ${ENV}"
-
-sh /opt/git/repos/data-iconic/${ENV}/redshift/shell/cls_insert.sh
