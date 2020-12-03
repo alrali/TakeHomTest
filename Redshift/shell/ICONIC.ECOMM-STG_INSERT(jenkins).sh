f71fdb3cc883e0f4adb5217ca24f3c6ccf8ce48b
alrali <75405592+alrali@users.noreply.github.com>
2020-12-03 14:37:25 +1100

    Delete ICONIC.ECOMM-STG_INSERT(jenkins).sh

--- Redshift/shell/ICONIC.ECOMM-STG_INSERT(jenkins).sh
+++
@@ -1,6 +0,0 @@
-#ENV=dev
-set +x
-################ convert the json to column ################
-echo "Environment name is : ${ENV}"
-
-sh /opt/git/repos/data-iconic/${ENV}/redshift/shell/stg_insert.sh
