41cba52b7c9b2831f84a24f537fbd5a1ff980019
alrali <75405592+alrali@users.noreply.github.com>
2020-12-03 14:38:01 +1100

    Delete ICONIC.ECOMM-TEMP_INSERT(jenkins).sh

--- Redshift/shell/ICONIC.ECOMM-TEMP_INSERT(jenkins).sh
+++
@@ -1,6 +0,0 @@
-#ENV=dev
-set +x
-################ Load the raw json data into the temp table ################
-echo "Environment name is : ${ENV}"
-
-sh /opt/git/repos/data-iconic/${ENV}/redshift/shell/temp_insert.sh
